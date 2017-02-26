a27ca859cb09ddd7ed7453f859c5ca9b973e5e4c
Domn Werner <wenr0096@vandals.uidaho.edu>
2017-02-26 15:27:55 -0800

    Finished prototype.

--- PhotoChemistry.Application/Program.cs
+++ PhotoChemistry.Application/Program.cs
@@ -1,15 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PhotoChemistry.Library;
 
 namespace PhotoChemistry.Application
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
+            SerialPort p = new SerialPort("COM6", 9600);
+            p.Open();
+            if (p.IsOpen)
+            {
+                Console.WriteLine("Connection established.");
+            }
+            p.WriteLine("0,0,0,");
+            while (true)
+            {
+                Console.Write("Enter a wavelength: ");
+                string input = Console.ReadLine();
+                double wavelength = double.Parse(input);
+                byte[] rgb = WavelengthToRgbCalculator.Calc(wavelength);
+                StringBuilder sb = new StringBuilder();
+                foreach (byte b in rgb)
+                {
+                    sb.Append($"{b},");
+                }
+                Debug.WriteLine($"Writing {sb}");
+                p.WriteLine(sb.ToString());
+            }
         }
     }
 }
